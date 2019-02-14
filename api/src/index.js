108e9697089e52cbd652e5b1c322bbd168aa1b77
Damilola Adekoya <dharmykoya38@gmail.com>
2019-02-14 23:07:17 +0100

    mobie and ipad views

--- api/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import mealsRoute from './routes/mealsRoute';
-
-const app = express();
-
-app.use('/api/v1/meals', mealsRoute);
-
-const PORT = 8080;
-app.listen(PORT);
